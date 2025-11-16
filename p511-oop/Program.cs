9d14218ffc41b66929f31fa4bed6d85a664532d2
Charlssssss33 <archernov2010@gmail.com>
2025-11-16 13:36:48 +0300

    completed

--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -5,19 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace p511_oop
-{
+{ 
+    public class Student
+    {
+        public string firstName;
+        public string lastname;
+        public int age;
+
+        public void Introduce() 
+        {
+            Console.WriteLine($"Пивет, меня зовут {firstName} {lastname},мне {age} лет");
+        }
+    }
+
     internal class Program
     {
-        static int add(int a , int b)
-        {
-            return a + b;
-        }
+   
 
 
 
         static void Main(string[] args)
         {
-            add(1, 2);
+           Student student = new Student();
+            student.firstName = "Иван";
+            student.lastname = "Петров";
+            student.age = 19;
+
+            Student student1 = new Student();
+            student1.firstName = "Анна";
+            student1.lastname = "Сидорова";
+            student1.age = 20;
+            Console.WriteLine("--- Знакомство со студентами ---");
+            student.Introduce();
+            student1.Introduce();
         }
     }
 }
