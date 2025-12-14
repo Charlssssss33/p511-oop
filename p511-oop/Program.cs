858318ef6bc9915390c1095ad24bab2ff534c1ab
Charlssssss33 <archernov2010@gmail.com>
2025-12-14 14:59:37 +0300

    fffhhhfh

--- p511-oop/Program.cs
+++ p511-oop/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace p511_oop
 { 
@@ -13,12 +14,18 @@ namespace p511_oop
     {
         static void Main(string[] args)
         {
-            List<Transport> transports = new List<Transprt>()
+           try
            {
-               new Car(),
-           }; 
-
+                string text = File.ReadAllText("data.txt");
+                Console.WriteLine(text);
+           }catch(UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Нет прав на открытие файлов")
+            }
            
+
+           }
+          
         }
     }
 }
